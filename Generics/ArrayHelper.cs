6483ceb8cf41dc3b4611516d9d8bce5d66614492
george-stroescu <geo_stroescu@yahoo.com>
2020-06-15 19:07:36 +0300

    Done

---
+++ Generics/ArrayHelper.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Generics
+{
+    class ArrayHelper<T>
+        where T: IEquatable<T>
+    {
+        public ArrayHelper()
+        {
+
+        }
+
+        public int ReturnIndexOfElement(T[] array, T element)
+        {
+            int arrayLength = array.Length;
+
+            for (int i = 0; i < arrayLength; i++)
+            {
+                if (array[i].Equals(element))
+                    return i;
+            }
+
+            return -1;
+        }
+
+        public bool SearchResult<T>(T[] array, int index, out T element)
+        {
+            if (index < array.Length)
+            {
+                element = array[index];
+                return true;
+            }
+
+            element = default;
+            return false;
+        }
+
+        public T[] ReturnSubArray(T[] array, int startIndex, int subArrayLength)
+        {
+            int length = array.Length;
+
+            if (length <= startIndex)
+                return default;
+
+            if (length < startIndex + subArrayLength)
+                subArrayLength = length - startIndex;
+
+            T[] subArray = new T[subArrayLength];
+
+            for (int i = 0; i < subArrayLength; i++)
+            {
+                subArray[i] = array[startIndex];
+                startIndex++;
+            }
+
+            return subArray;
+        }
+
+        public T[] ArraySort<T>(T[] array)
+            where T: IComparable<T>
+        {
+            int length = array.Length;
+
+            bool changed = false;
+
+            do
+            {
+                changed = false;
+
+                for (int i = 0; i < length; i++)
+                {
+                    if (i.Equals(length - 1))
+                        continue;
+
+                    int result = array[i].CompareTo(array[i + 1]);
+
+                    switch (result)
+                    {
+                        case 0:
+                        case -1:
+                            continue;
+                        case 1:
+                            T temp = array[i];
+                            array[i] = array[i + 1];
+                            array[i + 1] = temp;
+                            changed = true;
+                            break;
+                    }
+                }
+            }
+            while (changed);
+
+            return array;
+        }
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayHelper<int> arrayHelper = new ArrayHelper<int>();
+
+            bool success;
+
+            int[] intArray = { 1, 642, 135, 6583, 2, 64, 2, 67 };
+            int intElement;
+
+            string[] stringArray = { "agad", "24yedqawg", "g53q", "35yg" };
+            string stringElement;
+
+            int[] subArray = arrayHelper.ReturnSubArray(intArray, 6, 2);
+
+            Console.WriteLine($"ReturnIndex: {arrayHelper.ReturnIndexOfElement(intArray, 6583)}");
+            Console.WriteLine($"ReturnIndex: {arrayHelper.ReturnIndexOfElement(intArray, 333333)}");
+
+            success = arrayHelper.SearchResult<int>(intArray, 8, out intElement);
+            Console.WriteLine($"SearchResult: {success}, {intElement}");
+
+            success = arrayHelper.SearchResult<string>(stringArray, 0, out stringElement);
+            Console.WriteLine($"SearchResult: {success}, {stringElement}");
+
+            Console.Write("SubArray: ");
+            if (subArray is null)
+            {
+                Console.WriteLine("NULL");
+            }
+            else
+            {
+                for (int i = 0; i < subArray.Length; i++)
+                {
+                    Console.Write($"{subArray[i]}, ");
+                }
+                Console.WriteLine();
+            }
+
+            int[] sortedArray = arrayHelper.ArraySort<int>(intArray);
+
+            Console.Write("Sorted Array: ");
+            for (int i = 0; i < sortedArray.Length; i++)
+            {
+                Console.Write($"{sortedArray[i]}, ");
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
